16571c7ebcb2ee03700b9c0378a2833a986cc098
Anshumaan Mishra <anshumaan@localhost.localdomain>
2025-07-11 19:16:20 +0530

    Sync

--- fish/config.fish
+++ fish/config.fish
@@ -6,8 +6,11 @@ alias l="ls -la"
 alias zen=/usr/local/bin/launch-zen
 alias cd="z"
 alias cdi="zi"
-# zxoide ZSH
-set PATH "$HOME/.local/bin:$PATH"
-set -gx PATH /usr/bin $PATH
+alias zse="sudo zypper se"
+alias zsep="sudo zypper se --provides"
+alias zin="sudo zypper in"
+alias zrm="sudo zypper rm"
+alias zup="sudo zypper dup && sudo zypper update"
+alias zrf="sudo zypper refresh"
 
 eval "$(zoxide init fish)"
